8db0e09fd76e51a2cd135872980ce9d0a824905a
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-06-23 14:18:55 +0100

    Update tests

--- test/nft-test.js
+++ test/nft-test.js
@@ -1,10 +1,10 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("GemNFT", function () {
+describe("MultaVerse", function () {
   this.timeout(50000);
 
-  let gemNFT;
+  let multaVerse;
   let owner;
   let acc1;
   let acc2;
@@ -13,39 +13,39 @@ describe("GemNFT", function () {
   this.beforeEach(async function() {
       // This is executed before each test
       // Deploying the smart contract
-      const GemNFT = await ethers.getContractFactory("GemNFT");
+      const MultaVerse = await ethers.getContractFactory("MultaVerse");
       [owner, acc1, acc2] = await ethers.getSigners();
 
-      gemNFT = await GemNFT.deploy();
-      await gemNFT.deployed();
-      contractAddress = gemNFT.address;
+      multaVerse = await MultaVerse.deploy();
+      await multaVerse.deployed();
+      contractAddress = multaVerse.address;
   })
 
   it("Should set the right owner", async function () {
-      expect(await gemNFT.owner()).to.equal(owner.address);
+      expect(await multaVerse.owner()).to.equal(owner.address);
   });
 
   it("Should mint one NFT", async function() {
-      expect(await gemNFT.balanceOf(contractAddress)).to.equal(0);
+      expect(await multaVerse.balanceOf(contractAddress)).to.equal(0);
       
       const tokenValue = 100;
       const tokenURI = "https://example.com";
-      const tx = await gemNFT.connect(owner).mintToken(tokenURI, tokenValue);
+      const tx = await multaVerse.connect(owner).mintToken(tokenURI, tokenValue);
       await tx.wait();
 
-      expect(await gemNFT.balanceOf(contractAddress)).to.equal(1);
+      expect(await multaVerse.balanceOf(contractAddress)).to.equal(1);
   })
 
   it("Should set the correct tokenURI", async function() {
       const tokenURI_1 = "https://example.com/1"
       const tokenURI_2 = "https://example.com/2"
 
-      const tx1 = await gemNFT.connect(owner).mintToken(tokenURI_1, 100);
+      const tx1 = await multaVerse.connect(owner).mintToken(tokenURI_1, 100);
       await tx1.wait();
-      const tx2 = await gemNFT.connect(owner).mintToken(tokenURI_2, 200);
+      const tx2 = await multaVerse.connect(owner).mintToken(tokenURI_2, 200);
       await tx2.wait();
 
-      expect(await gemNFT.tokenURI(0)).to.equal(tokenURI_1);
-      expect(await gemNFT.tokenURI(1)).to.equal(tokenURI_2);
+      expect(await multaVerse.tokenURI(0)).to.equal(tokenURI_1);
+      expect(await multaVerse.tokenURI(1)).to.equal(tokenURI_2);
   })
 });
